815fc25ac0acff898e4cafa0acf7a54d6823c7c2
finnlay-gcp <finnlaypullen@gmail.com>
2025-11-22 11:24:43 +0000

    feat: Canny edge detection and gaussian blur

--- CV_Python_template.py
+++ CV_Python_template.py
@@ -13,13 +13,20 @@ cap = cv2.VideoCapture(1)
 cap.set(3,1920)
 cap.set(4,1080)
 
-#Create two opencv named windows
-cv2.namedWindow("frame-image", cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow("gray-image", cv2.WINDOW_AUTOSIZE)
+# Create three opencv named windows with half the screen size
+window_width = 768
+window_height = 432
+cv2.namedWindow("frame-image", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("frame-image", window_width, window_height)
+cv2.namedWindow("gray-image", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("gray-image", window_width, window_height)
+cv2.namedWindow("canny-image", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("canny-image", window_width, window_height)
 
-#Position the windows next to eachother
-cv2.moveWindow("frame-image",0,100)
-cv2.moveWindow("gray-image",640,100)
+# Position the windows in quadrants of the screen
+cv2.moveWindow("frame-image", 0, 0)
+cv2.moveWindow("gray-image", 780, 0)
+cv2.moveWindow("canny-image", 0, 510)
 
 # Execute this continuously
 while(True):
@@ -32,14 +39,22 @@ while(True):
     
     # Convert the image from the camera to Gray scale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+
+    # Example of additional image processing: Gaussian Blur
+    gauss = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Example of additional image processing: Canny Edge Detection
+    canny = cv2.Canny(gauss, 75, 100)
+
     # Display the original frame in a window
     cv2.imshow('frame-image',frame)
-
     
     # Display the grey image in another window
     cv2.imshow('gray-image',gray)
     
+    # Display the canny image in another window
+    cv2.imshow('canny-image',canny)
+
     # Stop the performance counter
     end = time.perf_counter()
     
